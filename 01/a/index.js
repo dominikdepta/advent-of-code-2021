163aaf6328e094e3804f6cb68d8e2a2bb41b2421
Dominik Depta <dominikdepta@gmail.com>
2021-12-05 16:10:32 +0100

    solve day 1 - part 1

---
+++ 01/a/index.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+const readline = require("readline");
+
+async function main() {
+  const fileLines = readline.createInterface({
+    input: fs.createReadStream(path.join(__dirname, "input.txt")),
+  });
+
+  let prevValue = null;
+  let currValue = null;
+  let result = 0;
+
+  try {
+    for await (const line of fileLines) {
+      if (!prevValue) {
+        prevValue = line;
+      } else {
+        prevValue = currValue;
+        currValue = line;
+      }
+
+      result = result + (prevValue < currValue);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+
+  console.log(result);
+}
+
+main();
