112da7231de448bdf341d7d1c16b8c1dcad3d40c
Dominik Depta <dominikdepta@gmail.com>
2021-12-08 18:20:02 +0100

    format

--- 03/a/index.js
+++ 03/a/index.js
@@ -9,13 +9,18 @@ const digits = fs
 
 const digitColumns = digits[0].map((_, i) => digits.map((row) => row[i]));
 
-const columnsAverage = digitColumns.map((col) => {
-  return col.reduce((acc, d) => acc + d, 0) / col.length
-});
+const columnsAverage = digitColumns.map(
+  (col) => col.reduce((acc, d) => acc + d, 0) / col.length
+);
 
-const gammaRateBinary = columnsAverage.map((avg) => avg > 0.5 ? 1 : 0).join('');
-const epsilonRateBinary = columnsAverage.map((avg) => avg < 0.5 ? 1 : 0).join('');
+const gammaRateBinary = columnsAverage
+  .map((avg) => (avg > 0.5 ? 1 : 0))
+  .join("");
+const epsilonRateBinary = columnsAverage
+  .map((avg) => (avg < 0.5 ? 1 : 0))
+  .join("");
 
-const powerConsumptionDecimal = parseInt(gammaRateBinary, 2) * parseInt(epsilonRateBinary, 2);
+const powerConsumptionDecimal =
+  parseInt(gammaRateBinary, 2) * parseInt(epsilonRateBinary, 2);
 
 console.log(powerConsumptionDecimal);
